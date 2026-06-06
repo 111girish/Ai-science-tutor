9637f3b5cc5a691420646afc4513a72858bf7aaf
111girish <pokhrelgirish94@gmail.com>
2026-06-06 19:03:08 +0545

    Created authentication

---
+++ server/middleware/authentication.js
@@ -0,0 +1,25 @@
+import getEnv from "../config.js";
+
+
+const authentication = (req, res, next) => {
+  const authHeader = req.headers['authentication'];
+
+  if (!authHeader) return res.send(401).json({message: "Authentication failed!!"});
+
+  const token = authHeader.split(' ')[1] || '';
+  const secret = getEnv('accessToken');
+
+  try{
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded;
+    next();
+  } 
+  catch(err) {
+
+    console.log(err);
+    res.status(401).json({message: "Authentication failed"});
+  } 
+}
+
+export default authentication;
+
