82053f3770bb09212143d1b29c23fdf496dd8318
111girish <pokhrelgirish94@gmail.com>
2026-06-09 11:28:18 +0545

    Finished linking pool

--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -1,5 +1,8 @@
 import pool from "../db.js";
+import {useState} from "react";
+import { useParams } from "react-router-dom";
 
+const params = useParams();
 export const getConvo = (req, res) => {
   
   const client =await pool.connect();
@@ -13,14 +16,36 @@ export const getConvo = (req, res) => {
     client.release();
   }
 
-
-  return (
-    <>
-      <div></div>
-    </>
-  );
 };
 
-export const postConvo = (req, res) => {};
+export const postConvo = (req, res) => {
 
-export const deleteConvo = (req, res) => {};
+  const client = pool.connect();
+  try {
+    const result = client.query(text, value);
+    res.status(200).json({message:"There conversation is added"})
+  } catch(err) {
+    console.log(err);
+    res.status(400).json({message:"There seems to be an error"})
+  } finally{
+    client.release();
+  }
+};
+
+export const deleteConvo = (req, res) => {
+  const {convoId} = req.params();
+
+  const client = pool.connect();
+  try {
+    const result = client.query(text, value);
+    res.status(200).json({message: "The conversation has been deleted"})
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({message: "There seens to be an error"});
+
+  } finally {
+    client.release();
+  }
+
+
+};
