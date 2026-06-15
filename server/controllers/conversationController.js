c67a6987805bc8193facfeb545ae4d06c7ccf19b
111girish <pokhrelgirish94@gmail.com>
2026-06-15 22:44:35 +0545

    Finished creating getConvo and postConvo and deleteConvo

--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -20,15 +20,21 @@ export const getConvo = async (req, res) => {
   }finally{
     client.release();
   }
-
 };
 
 export const postConvo = (req, res) => {
+  const userId = req.user.userId;
+
+  const {subjectId, title} = req.body;
+
+  const text = "INSERT INTO conversations(title, subjectId, userId) VALUES($1, $2, $3) RETURNING * ;";
+  const values = [title, subjectId, userId];
 
   const client = pool.connect();
   try {
     const result = client.query(text, value);
-    res.status(200).json({message:"There conversation is added"})
+    const data = result.rows[0];
+    res.status(200).json({message:"There conversation is added", data});
   } catch(err) {
     console.log(err);
     res.status(400).json({message:"There seems to be an error"})
@@ -39,6 +45,10 @@ export const postConvo = (req, res) => {
 
 export const deleteConvo = (req, res) => {
   const {convoId} = req.params();
+  const userId = req.user.userId;
+
+  const text = "DELETE FROM conversations WHERE convoId = $1 AND userId = $2";
+  const values = [convoId, userId];
 
   const client = pool.connect();
   try {
@@ -51,6 +61,4 @@ export const deleteConvo = (req, res) => {
   } finally {
     client.release();
   }
-
-
 };
