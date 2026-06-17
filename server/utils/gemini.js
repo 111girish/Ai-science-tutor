01fb74eccd2fec8e350f6e4f85749996a3090ce7
111girish <pokhrelgirish94@gmail.com>
2026-06-17 22:58:56 +0545

    Learned how to use .map() in a new way

--- server/utils/gemini.js
+++ server/utils/gemini.js
@@ -1,6 +1,14 @@
 import getEnv from "../config.js";
 
-const askGemini = (subject, history, userMessage) => {
-  const apiKey = getEnv('geminiApiKey');
+const askGemini = async (subject, history, userMessage) => {
+  const apiKey = getEnv("geminiApiKey");
 
-}
+  const geminiHistory = history.map((hist) => {
+    return {
+      role: hist.sender ==="ai" ? "model" : "user",
+      parts: [{text: hist.content}]
+    }
+  })
+};
+
+export default askGemini;
