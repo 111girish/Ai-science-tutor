26aab7f2ff9a19a2c4ef4f567a4bec13970bcaaf
111girish <pokhrelgirish94@gmail.com>
2026-06-19 22:57:24 +0545

    PostMessage half finished

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,5 @@
 import pool from '../db.js';
+import askGemini from '../utils/gemini.js';
 
 export const getMessage = async (req, res) => {
   const userId = req.user.userId;
@@ -28,5 +29,25 @@ export const postMessage = async (req, res) => {
 
   const {userMessage} = req.body;
 
-  const text1 = "SELECT * FROM conversations "
+  const text1 = "SELECT subject_id FROM conversations where conversation_id = $1";
+  const value1 = [convoId];
+  const client = await pool.connect();
+  try {
+    const response1 = await client.query(text1, value1);
+    const subjectId = response1.rows[0].subject_id;
+    const text2 = "SELECT subject FROM subjects WHERE subject_id = $1";
+    const response2 = await client.query(text2, [subjectId]);
+    const subject = response2.rows[0].subject;
+    const text3 = "SELECT content FROM messages WHERE conversation_id = $1 ORDER BY created_at ASC";
+    const value3 = [convoId];
+    const response = await client.query(text3, value3);
+    const history = response.rows;
+    const AIresponse = await askGemini(subject, history, userMessage);
+
+  }catch(err){
+    console.log(err);
+    res.status(400).json({message: "There seems to be an error!!!"})
+  } finally{
+    client.release();
+  }
 }
