c72b7a540f2ce5ece25d57e3287f71206eed1a51
111girish <pokhrelgirish94@gmail.com>
2026-06-20 23:09:32 +0545

    Finished creating postConversations

--- client/src/api/conversations.js
+++ client/src/api/conversations.js
@@ -1,9 +1,20 @@
 import axios from "axios"
 
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 export const getConversations =async () => {
   const token = localStorage.getItem('token');
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/conversations/`, {
+  const response = await axios.get(`${BASE_URL}/api/conversations/`, {
     headers: {Authorization: `Bearer ${token}`}
   }) 
+  return response.data;
+}
+
+export const postConversations = async (title, subjectId) => {
+  const token = localStorage.getItem('token');
+  const convoData = {title, subjectId};
+  const response = await axios.post(`${BASE_URL}/api/conversations`, convoData, {
+    headers: {Authorization: `Bearer ${token}`}
+  })
   return response.data;
 }
