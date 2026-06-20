f0e3d627ea5328bec983939d12bf199ebe769f17
111girish <pokhrelgirish94@gmail.com>
2026-06-20 17:23:55 +0545

    Installed axios and finished creating auth.js

---
+++ client/src/api/auth.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_URL
+export const loginUser =async (username, email, password) => {
+  const Logindata = {user_name: username, email, password};
+  try{
+    const response = await axios.post(`${BASE_URL}/login`, Logindata);
+    localStorage.setItem("token", response.data.token);
+    return response.data;
+  } catch(err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export const registerUser =async (fullname, username, email, password) => {
+  const registerData = {full_name: fullname, user_name: username, email, password};
+  try{
+    const response =await axios.post(`${BASE_URL}/register`, registerData);
+    return response.data;
+  } catch(err){
+    console.log(err);
+    throw err;
+  }
+}
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,16 @@
-const Login =() =>{
-  return(
+import { loginUser } from "../api/auth.js";
+
+const Login = () => {
+  const [loginData, setLoginData] = useState({username: "", email:"", password: ""});
+  
+  return (
     <>
       <p>Login page</p>
-
+      <input onChange={handleChange} placeholder="username"/>
+      <input placeholder="password" />
+      <input type="submit" />
     </>
   );
-}
+};
 
 export default Login;
