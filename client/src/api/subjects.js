2679480dbc61e01f96c49afcbdce1a330a881443
111girish <pokhrelgirish94@gmail.com>
2026-06-20 22:59:49 +0545

    Finished creating getSubjects

---
+++ client/src/api/subjects.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getSubjects = async () => {
+  const token = localStorage.getItem('token');
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/subjects`, {
+    headers: {Authorization: `Bearer ${token}`}});
+  return response;
+}
