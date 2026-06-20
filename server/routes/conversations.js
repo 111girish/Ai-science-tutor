6d9e818abb84e2acebf8ff81e74f6aceb05d66fb
111girish <pokhrelgirish94@gmail.com>
2026-06-20 10:58:30 +0545

    Finished creating routes as well

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import authentication from '../middleware/authentication.js';
 import { deleteConvo, getConvo, postConvo } from '../controllers/conversationController.js';
+import { getMessage, postMessage } from '../controllers/messageController.js';
 const conversations = express.Router();
 
 conversations.get('/conversations', authentication, getConvo);
 conversations.post('/conversations', authentication, postConvo);
 conversations.delete('/conversations/:convoId', authentication, deleteConvo);
+conversations.post('conversations/:convoId/messages', authentication, postMessage);
+conversations.get('/conversations/:convoId/messages', authentication, getMessage);
 
 export default conversations;
