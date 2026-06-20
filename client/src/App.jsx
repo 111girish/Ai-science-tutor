a4a0967f4d123efc48bebd098d4663a96f13ec42
111girish <pokhrelgirish94@gmail.com>
2026-06-20 16:23:54 +0545

    Imported everything into App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Login from './pages/Login'
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Chat from './pages/Chat';
+import Homepage from './pages/Homepage';
 
 function App() {
 
   return (
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage />} /> 
 
+        <Route path='/login' element={<Login />} /> 
+        <Route path='/register' element={<Register />} /> 
+        <Route path='/dashboard' element={<Dashboard />} /> 
+        <Route path='/chat' element={<Chat />} /> 
+
+      </Routes>
+    </>
   )
 }
 
