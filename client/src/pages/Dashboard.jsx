daff1bcbc59a9b56b1a99fb4025cefb20b0639f3
111girish <pokhrelgirish94@gmail.com>
2026-06-22 00:27:59 +0545

    Now the conversation also loads in the dashboard at the start

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import axios from 'axios';
+// import axios from 'axios';
 import { getSubjects } from "../api/subjects";
+import { getConversations } from "../api/conversations";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
 
 const Dashboard =() =>{
 
@@ -15,7 +16,16 @@ const Dashboard =() =>{
         console.log(err);
       }
     }
+    const convoGet = async () => {
+      try{
+        const response = await getConversations();
+        return response.data;
+      } catch(err) {
+        console.log(err);
+      }
+    }
     subjectGet();
+    convoGet();
   }, [])
 
 
