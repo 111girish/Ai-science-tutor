4be85f20d3d883b9aa4d5045f102015f2b7af34a
111girish <pokhrelgirish94@gmail.com>
2026-06-22 00:22:28 +0545

    Now the subject is loaded on the startup

--- client/src/api/subjects.js
+++ client/src/api/subjects.js
@@ -4,5 +4,5 @@ export const getSubjects = async () => {
   const token = localStorage.getItem('token');
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/subjects`, {
     headers: {Authorization: `Bearer ${token}`}});
-  return response;
+  return response.data;
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
 import { useEffect } from "react";
+import axios from 'axios';
+import { getSubjects } from "../api/subjects";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const Dashboard =() =>{
+
+  useEffect(() => {
+    const subjectGet = async () => {
+      try{
+      const response = await getSubjects();
+      return response.data;
+      } catch(err){
+        console.log(err);
+      }
+    }
+    subjectGet();
+  }, [])
 
 
 
@@ -8,6 +24,11 @@ const Dashboard =() =>{
     <>
       <p>Dashboard page</p>
       <input placeholder="conversation title" />
+      <select>
+        <option>
+
+        </option>
+      </select>
     </>
   );
 }
