175af010a807ecedc79b2c36495d0d37e7551270
111girish <pokhrelgirish94@gmail.com>
2026-06-23 11:45:26 +0545

    Now the conversations gets displayed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,39 +1,37 @@
 import { useEffect, useState } from "react";
 // import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 import { getSubjects } from "../api/subjects";
 import { getConversations } from "../api/conversations";
 
 // const BASE_URL = import.meta.env.VITE_API_URL;
-
-const Dashboard =() =>{
-
+const navigate = useNavigate;
+const Dashboard = () => {
   const [subjects, setSubjects] = useState([]);
   const [conversations, setConversations] = useState([]);
 
   useEffect(() => {
     const subjectGet = async () => {
-      try{
-      const response = await getSubjects();
-      setSubjects(response.data);
-      } catch(err){
+      try {
+        const response = await getSubjects();
+        setSubjects(response.data);
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
     const convoGet = async () => {
-      try{
+      try {
         const response = await getConversations();
         setConversations(response.data);
-      } catch(err) {
+      } catch (err) {
         console.log(err);
       }
-    }
+    };
     subjectGet();
     convoGet();
-  }, [])
+  }, []);
 
-
-
-  return(
+  return (
     <>
       <p>Dashboard page</p>
       <input placeholder="conversation title" />
@@ -44,8 +42,18 @@ const Dashboard =() =>{
           </option>
         ))}
       </select>
+      <div>
+        {conversations.map((conversation) => (
+          <div
+            key={conversation.conversation_id}
+            onClick={() => navigate(`chat/${conversation.conversation_id}`)}
+          >
+            <p>{conversation.title}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
-}
+};
 
 export default Dashboard;
